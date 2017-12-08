de66d6db3f3ff9ff804ecc16512e1ec32a2ab5ed
TanPhat <TanPhat@192.168.214.1>
2017-12-08 16:55:45 +0700

    sua lan dau

--- DOAN_TTTH/src/Controls/Login.java
+++ DOAN_TTTH/src/Controls/Login.java
@@ -20,7 +20,7 @@ public class Login extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		uguhhbhjbhjbhjbhjbhjbhjb
 	}
 
 	
